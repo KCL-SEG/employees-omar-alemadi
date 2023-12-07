a6a1fb1495fb59f9b9e2f2ea1b931dbcbf697d6b
Omar Al-Emadi <k21043327@kcl.ac.uk>
2023-12-07 03:34:38 +0000

    tt

--- employee.py
+++ employee.py
@@ -1,4 +1,4 @@
-import re
+"""Employee pay calculator."""
 
 class Employee:
     def __init__(self, name):
@@ -38,58 +38,3 @@ class HourlySalaryEmployee(Employee):
 
     def __str__(self):
         return f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_wage}/hour. Their total pay is {self.get_pay()}."
-
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = MonthlySalaryEmployee('Billie', 4000)
-
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = HourlySalaryEmployee('Charlie', 25, 100)
-
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = MonthlySalaryEmployee('Renee', 3000, contracts=4, commission_per_contract=200)
-
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = HourlySalaryEmployee('Jan', 25, 150, contracts=3, commission_per_contract=220)
-
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = MonthlySalaryEmployee('Robbie', 2000, bonus=1500)
-
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = HourlySalaryEmployee('Ariel', 30, 120, bonus=600)
-
-# Testing
-def test_billie():
-    assert billie.get_pay() == 4000
-    string = str(billie)
-    regex = '^Billie works on a monthly salary of 4000. Their total pay is 4000.$'
-    assert re.search(regex, string)
-
-def test_charlie():
-    assert charlie.get_pay() == 2500
-    string = str(charlie)
-    regex = '^Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500.$'
-    assert re.search(regex, string)
-
-def test_renee():
-    assert renee.get_pay() == 3800
-    string = str(renee)
-    regex = '^Renee works on a monthly salary of 3000 and receives a commission for 4 contract\(s\) at 200/contract. Their total pay is 3800.$'
-    assert re.search(regex, string)
-
-def test_jan():
-    assert jan.get_pay() == 4410
-    string = str(jan)
-    regex = '^Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract\(s\) at 220/contract. Their total pay is 4410.$'
-    assert re.search(regex, string)
-
-def test_robbie():
-    assert robbie.get_pay() == 3500
-    string = str(robbie)
-    regex = '^Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500.$'
-    assert re.search(regex, string)
-
-def test_ariel():
-    assert ariel.get_pay() == 4200
-    string = str(ariel)
-    regex = '^Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200.$'
-    assert re.search(regex, string)
