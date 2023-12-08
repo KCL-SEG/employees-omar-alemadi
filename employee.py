7be1789c0c5a79f0fc5b346e46d6edd6e4eb91ff
Omar Al-Emadi <k21043327@kcl.ac.uk>
2023-12-08 03:03:41 +0000

    pushed code

--- employee.py
+++ employee.py
@@ -1,50 +1,71 @@
-"""Employee pay calculator."""
-
-import re  # Add this import for using re.match
-
 class Employee:
     def __init__(self, name):
         self.name = name
 
     def get_pay(self):
-        raise NotImplementedError("Subclasses must implement get_pay method")
+        pass
 
     def __str__(self):
-        raise NotImplementedError("Subclasses must implement __str__ method")
+        return self.name
 
-class MonthlySalaryEmployee(Employee):
-    def __init__(self, name, monthly_salary, bonus=0, contracts=0, commission_per_contract=0):
+
+class MonthlyEmployee(Employee):
+    def __init__(self, name, monthly_salary):
         super().__init__(name)
         self.monthly_salary = monthly_salary
-        self.bonus = bonus
-        self.contracts = contracts
-        self.commission_per_contract = commission_per_contract
 
     def get_pay(self):
-        return self.monthly_salary + self.bonus + (self.contracts * self.commission_per_contract)
+        return self.monthly_salary
 
     def __str__(self):
-        return f"{self.name} works on a monthly salary of {self.monthly_salary}. Their total pay is {self.get_pay()}."
+        return f"{super().__str__()} works on a monthly salary of {self.monthly_salary}. Their total pay is {self.get_pay()}."
 
-class HourlySalaryEmployee(Employee):
-    def __init__(self, name, hourly_wage, hours_worked, bonus=0, contracts=0, commission_per_contract=0):
+
+class ContractEmployee(Employee):
+    def __init__(self, name, hours, rate):
         super().__init__(name)
-        self.hourly_wage = hourly_wage
-        self.hours_worked = hours_worked
-        self.bonus = bonus
-        self.contracts = contracts
-        self.commission_per_contract = commission_per_contract
+        self.hours = hours
+        self.rate = rate
 
     def get_pay(self):
-        return (self.hourly_wage * self.hours_worked) + self.bonus + (self.contracts * self.commission_per_contract)
+        return self.hours * self.rate
 
     def __str__(self):
-        return f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_wage}/hour. Their total pay is {self.get_pay()}."
+        return f"{super().__str__()} works on a contract of {self.hours} hours at {self.rate}/hour. Their total pay is {self.get_pay()}."
 
-# Create instances for employees
-billie = MonthlySalaryEmployee("Billie", monthly_salary=4000)
-charlie = HourlySalaryEmployee("Charlie", hourly_wage=25, hours_worked=100)
-renee = MonthlySalaryEmployee("Renee", monthly_salary=3000, contracts=4, commission_per_contract=200)
-jan = HourlySalaryEmployee("Jan", hourly_wage=25, hours_worked=150, contracts=3, commission_per_contract=220)
-robbie = MonthlySalaryEmployee("Robbie", monthly_salary=2000, bonus=1500)
-ariel = HourlySalaryEmployee("Ariel", hourly_wage=30, hours_worked=120, bonus=600)
+
+class CommissionedEmployee(MonthlyEmployee):
+    def __init__(self, name, monthly_salary, commission_rate, num_contracts):
+        super().__init__(name, monthly_salary)
+        self.commission_rate = commission_rate
+        self.num_contracts = num_contracts
+
+    def get_pay(self):
+        return super().get_pay() + (self.commission_rate * self.num_contracts)
+
+    def __str__(self):
+        return f"{super().__str__()} and receives a commission for {self.num_contracts} contract(s) at {self.commission_rate}/contract. Their total pay is {self.get_pay()}."
+
+
+class BonusCommissionedEmployee(MonthlyEmployee):
+    def __init__(self, name, monthly_salary, bonus_commission):
+        super().__init__(name, monthly_salary)
+        self.bonus_commission = bonus_commission
+
+    def get_pay(self):
+        return super().get_pay() + self.bonus_commission
+
+    def __str__(self):
+        return f"{super().__str__()} and receives a bonus commission of {self.bonus_commission}. Their total pay is {self.get_pay()}."
+
+
+# Create employee instances
+billie = MonthlyEmployee('Billie', 4000)
+charlie = ContractEmployee('Charlie', 100, 25)
+renee = CommissionedEmployee('Renee', 3000, 200, 4)
+jan = CommissionedEmployee('Jan', 0, 220, 3)  # Monthly salary set to 0
+robbie = BonusCommissionedEmployee('Robbie', 2000, 1500)
+ariel = BonusCommissionedEmployee('Ariel', 0, 600)  # Monthly salary set to 0
+
+
+
